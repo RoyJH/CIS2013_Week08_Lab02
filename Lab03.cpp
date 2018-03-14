3633176b34e9ac048956a42985d96bd5ea966cf2
RoyJH <royjh@okstate.edu>
2018-03-14 17:51:37 -0500

    Two-dimension Arrays

--- Lab03.cpp
+++ Lab03.cpp
@@ -3,19 +3,21 @@ using namespace std;
 
 int main(){
 
-	int numbers[10];
+	int numbers[10][10];
 	
-	for (int i=0; i<10; i++){
-		cout << "Enter number " << i << ": ";
-		cin >> numbers[i];
+	for (int x=0; x<10; x++){
+		for (int y=0; y<10; y++){
+		numbers [x][y] = 'X';
+		}
 	}
+		
+	cout << endl << "      0 1 2 3 4 5 6 7 8 9" << endl;
 	
-	// find average
-	int total=0;
-	for (int i=0; i<10; i++){
-	total = total + numbers[i];
+	for (int x=0; x<10; x++){
+		cout << "     " << x << "  ";
+		for (int y=0; y<10; y++){;
+		cout << numbers [x][y] << "  ";
+		}
 	}
-	cout << "Our total is : " << total << endl;
-	cout << "Our average is: " << total/10<< endl;
 	return 0;
 }
